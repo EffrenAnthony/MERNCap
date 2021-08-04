e2267da13f96b4acdd5171f7bb885a5b270f2e92
tony <effrenanthony2704@gmail.com>
2021-08-03 23:35:31 -0500

    javascript basic

--- condicionalesYLoops.js
+++ condicionalesYLoops.js
@@ -6,7 +6,7 @@ let numero = 0
 let booleano = true
 let float = 1.2
 
-// no primitivos
+// no primitivos - Objetos
 let arreglo = [1,2,43,5]
 let arreglo2 = new Array(1,2,4,5)
 let objetos = {
@@ -45,7 +45,103 @@ function saludar(saludo){
   // if (saludo === 'pedro') {
   //   console.log('saludo de pedro')
   // }
+  
 }
 
-saludar('pedro')
+// saludar('pedro')
 
+function switchCase(param){
+  switch(param){
+    case 'luciano':
+      return 'luciano'
+    case 'pedro':
+      return 'pedro'
+    case 'daniel':
+      let sum = 10+ 10
+      return 'daniel tiene ' + sum + 'años'
+    default:
+      console.log('no coincide')
+      return 'no coincide'
+  }
+}
+
+// const resultado = switchCase('tony')
+// console.log(resultado)
+
+
+// loops
+let contador = 0
+// while (contador < 10) {
+//   console.log(contador)
+//   // contador = contador + 1
+//   // contador++
+//   contador += 1
+// }
+
+// do{
+//   console.log(contador)
+//   // contador = contador + 1
+//   // contador++
+//   contador += 1
+// } while (contador < 10)
+
+// for (let index = 0; index < 10; index++) {
+//   console.log(index)
+// }
+
+// REFERENCIAS
+// let a = 1
+// let b = 2
+// let c = a + b
+// let d = a
+// a = 5
+
+// let array = [1,2,3,4]
+// let arrayNuevo = []
+// let array2 = array
+// array.push(5)
+
+// for(let i = 0; i < array.length; i++){
+//  arrayNuevo.push(array[i])
+// }
+
+// let arrayNuevoSpread = [...array]
+
+// let obj = {a: 1, b:2}
+// let obj2 = obj
+
+
+// for(let arr of arreglo){
+//   console.log(arr)
+// }
+let persona = {
+  nombre: "luciano",
+  edad: 26,
+  programador: true
+}
+// let valor = 15
+// console.log(`La edad de Lucia es ${valor + 5}`)
+// console.log(`${persona.nombre}tiene ${persona.edad} años y es ${persona.programador === true ? 'programador' : 'otra cosa'}`)
+
+// console.log(persona['nombre'])
+// console.log(Object.keys(persona));
+// console.log(Object.values(persona));
+
+let arregloPalabras = ["p1",'p2','p3','p4']
+// function callback(item){
+//   console.log(item)
+// }
+// arregloPalabras.forEach(callback)
+arregloPalabras.forEach(function (item){
+  console.log(item)
+})
+
+// map
+// reduce
+// filter
+// shift
+// unshift
+// pop
+// push
+// fill
+// sort
